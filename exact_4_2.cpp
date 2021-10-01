5120d6a50be41c665c59328dd39b628063857ce3
Anantha Sai Satwik Vysyaraju <saisatwik2226@gmail.com>
2021-10-01 15:15:03 +0530

    Update exact_4_2.cpp
    updated integer to boolean types

--- exact_4_2.cpp
+++ exact_4_2.cpp
@@ -1,20 +1,17 @@
 #include<bits/stdc++.h>
-#include<bitset>
-#include<iostream>
-
 #include "fullAdder.h"
 using namespace std;
 
-struct exact_4_2
+struct exact_4_2Struct
 {
-    int tout;
-    int carry;
-    int sum;
+    bool tout;
+    bool carry;
+    bool sum;
 };
 
-exact_4_2 exact_4_2Calc(int x4,int x3,int x2,int x1,int tin){
-    exact_4_2 s;
-    fullAdder f1,f2;
+exact_4_2Struct exact_4_2Calc(bool x4,bool x3,bool x2,bool x1,bool tin){
+    exact_4_2Struct s;
+    fullAdderStruct f1,f2;
     f1 = fullAdderCalc(x4,x3,x2);
     f2 = fullAdderCalc(tin,x1,f1.sum);
     s.tout = f1.carry;
@@ -22,3 +19,10 @@ exact_4_2 exact_4_2Calc(int x4,int x3,int x2,int x1,int tin){
     s.sum = f2.sum;
     return s;
 }
+
+// int main(){
+//     exact_4_2Struct ex;
+//     ex = exact_4_2Calc(1,1,1,1,1);
+//     cout<<ex.tout<<" "<<ex.carry<<" "<<ex.sum<<endl;
+//     return 0;
+// }
